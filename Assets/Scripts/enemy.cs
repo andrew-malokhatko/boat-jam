a88a5f2e068339f841baf1f81eaf4de08396f92d
yaroslav136 <mr.golov2014@gmail.com>
2025-02-18 18:05:38 +0100

    Implemented enemy movement mechanism using waypoint nodes

--- Assets/Scripts/enemy.cs
+++ Assets/Scripts/enemy.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Tilemaps;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] private List<Transform> points;
+    //[SerializeField] private List<Transform> points;
+    public Node currentNode;
     [SerializeField] private float speed;
     [SerializeField] private float climbSpeed;
+    public Tilemap tilemap;
 
     private Animator animator;
     private int currentIndex;
@@ -15,12 +18,16 @@ public class Enemy : MonoBehaviour
     private bool isDead;
     private bool isClimbing = false;
     private Rigidbody2D rb;
+    private bool isOnUp = false;
+    private bool isOnLadder = false;
+    private bool isNextPointOnLadder = false;
 
 
     void Start()
     {
         animator = GetComponent<Animator>();
-        currentPoint = points[0].position;
+        //currentPoint = points[0].position;
+        currentPoint = currentNode.transform.position;
         rb = GetComponent<Rigidbody2D>();
 
         ChooseDirection();
@@ -39,20 +46,36 @@ public class Enemy : MonoBehaviour
             //return; 
         }
 
-        Walk();
+        if (IsPointOnLadder(currentPoint))
+        {
+            isNextPointOnLadder = true;
+        }
+        else
+            isNextPointOnLadder = false;
 
-        if (Physics2D.OverlapCircle(transform.position, 0.5f, LayerMask.GetMask("Ladder")) && Vector3.Distance(transform.position, currentPoint) < 0.3f)
+        // starts climbing at ladder
+        if (isNextPointOnLadder && isOnLadder && Vector3.Distance(transform.position, currentPoint) < 0.15f)
         {
             isClimbing = true;
             ChooseNextPoint();
             StartClimbing();
         }
+        Walk();
 
         if (isClimbing)
         {
-            float vertical = Input.GetAxis("Vertical");
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, vertical * climbSpeed);
-            rb.gravityScale = 0;
+            // climbing to up
+            if (isOnUp)
+            {
+                rb.gravityScale = 0;
+                float step = climbSpeed * Time.deltaTime;
+                float newY = Mathf.MoveTowards(transform.position.y, currentPoint.y, step);
+                transform.position = new Vector2(transform.position.x, newY);
+            }
+            else
+            {
+                //fix
+            }
         }
         else
         {
@@ -60,25 +83,40 @@ public class Enemy : MonoBehaviour
         }
     }
 
+    public bool IsPointOnLadder(Vector3 worldPosition)
+    {
+        Vector3Int tilePosition = tilemap.WorldToCell(worldPosition);
+        return tilemap.HasTile(tilePosition);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Ladder"))
+        {
+            isOnLadder = true;
+        }
+    }
+
+    void OnTriggerLeft2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Ladder"))
+        {
+            isOnLadder = false;
+        }
+    }
+
     void StartClimbing()
     {
-        walking = false;
-        animator.SetBool("walk", false);
+        animator.SetBool("run", false);
         //fix
+        //walking = false;
         //animator.SetTrigger("climbing");
 
         // next code checks if next point under or over
-        bool isOnUp = false;
         if (transform.position.y < currentPoint.y)
             isOnUp = true;
         else
             isOnUp = false;
-            
-        // climbing to up
-        if (isOnUp)
-        {
-            //gameObject.
-        }
     }
 
     IEnumerator FadeAndDestroy()
@@ -96,8 +134,6 @@ public class Enemy : MonoBehaviour
             elapsedTime += Time.deltaTime;
             float alpha = Mathf.Lerp(1, 0, elapsedTime * 23 / fadeTime);
 
-            Debug.Log("Alpha: " + alpha);
-
             color.a = alpha;
             sprite.color = color;
             yield return null;
@@ -113,12 +149,22 @@ public class Enemy : MonoBehaviour
 
         if (walking)
         {
-            float step = speed * Time.deltaTime;
-            transform.position = Vector2.MoveTowards(transform.position, currentPoint, step);
+            float step = climbSpeed * Time.deltaTime;
+            float newX = Mathf.MoveTowards(transform.position.x, currentPoint.x, step);
+            transform.position = new Vector2(newX, transform.position.y);
 
-            if (Vector3.Distance(transform.position, currentPoint) < 0.3f)
+            if (Vector3.Distance(transform.position, currentPoint) < 0.2f && !isClimbing && !isNextPointOnLadder)
             {
                 StartCoroutine(Idle());
+            }
+            if (Vector3.Distance(transform.position, currentPoint) < 0.2f && isClimbing)
+            {
+                ChooseNextPoint();
+                isNextPointOnLadder = false;
+                isOnUp = false;
+                isOnLadder = false;
+                isClimbing = false;
+                rb.gravityScale = 1;
             }
         }
     }
@@ -129,15 +175,17 @@ public class Enemy : MonoBehaviour
         animator.SetTrigger("idle");
         ChooseNextPoint();
 
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(0.01f);
 
         walking = true;
     }
 
     private void ChooseNextPoint()
     {
-        currentIndex = ++currentIndex < points.Count ? currentIndex : 0;
-        currentPoint = points[currentIndex].position;
+        //currentIndex = ++currentIndex < points.Count ? currentIndex : 0;
+        //currentPoint = points[currentIndex].position;
+        currentNode = currentNode.nextPoint;
+        currentPoint = currentNode.transform.position;
 
         ChooseDirection();
     }
@@ -159,7 +207,7 @@ public class Enemy : MonoBehaviour
     public void Hit()
     {
         isDead = true;
-        animator.SetBool("walk", false);
+        animator.SetBool("run", false);
         animator.SetTrigger("dead");
         Destroy(GetComponent<Collider2D>(), 1);
         Destroy(GetComponent<Rigidbody2D>(), 1);
