0a907e61718ecba1e3cf516559ffce398c9f7e63
Andrew Malokhatko <andrew.malokhatko@gmail.com>
2025-02-21 13:10:33 +0200

    Steering Wheel

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,25 +5,79 @@ using System.Collections;
 public class Spawner : MonoBehaviour
 {
     public Hole hole;
+    public Wheel wheel;
+
+    // Max time for each object in seconds
+    private const float maxHoleTime = 25.0f;
+    private const float maxWheelTime = 12.0f;
+
+    private float timeSinceWheel = 0.0f;
+    private float timeSinceHole = 0.0f;
 
     private void Start()
     {
-        StartCoroutine(SpawnLoop());
+        StartCoroutine(HoleSpawnLoop());
+        StartCoroutine(SteeringActionLoop());
     }
 
-    private IEnumerator SpawnLoop()
+    private void Update()
     {
+        if (hole)
+        {
+            if (timeSinceHole > maxHoleTime)
+            {
+                Debug.Log("End game");
+                // Game End
+            }
+
+            timeSinceHole += Time.deltaTime;
+        }
+
+        if (wheel.requiresSteering)
+        {
+            if (timeSinceWheel > maxWheelTime)
+            {
+                Debug.Log("End game");
+                // Game end
+            }
+
+            timeSinceWheel += Time.deltaTime;
+        }
+    }
+
+    private IEnumerator HoleSpawnLoop()
+    {
+        // spawn first hole after 9 - 12 seconds from start
+        yield return new WaitForSeconds(Random.Range(12f, 17f));
+
         while (true)
         {
+            float waitTime = Random.Range(17f, 24f);
             SpawnObjects();
-            float waitTime = Random.Range(30f, 40f);
+
+            // spawn hole every 9 to 12 seconds
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+
+    private IEnumerator SteeringActionLoop()
+    {
+        // Require steering after 3 - 9 seconds from start
+        yield return new WaitForSeconds(Random.Range(2.5f, 5f));
+
+        while (true)
+        {
+            wheel.requiresSteering = true;
+
+            // require steering every 9 to 12 seconds
+            float waitTime = Random.Range(9f, 12f);
             yield return new WaitForSeconds(waitTime);
         }
     }
 
     private void SpawnObjects()
     {
-        Vector3 randomPos = new Vector3(Random.Range(-5f, 5f), Random.Range(-3.3f, -4f), 0);
+        Vector3 randomPos = new Vector3(Random.Range(-6f, 6f), Random.Range(-3.3f, -4f), 0);
 
         // spawn hole
         Instantiate(hole, randomPos, Quaternion.identity);
--- Assets/Scripts/Wheel.cs
+++ Assets/Scripts/Wheel.cs
@@ -1,23 +1,123 @@
+using System.Collections;
+using System.Linq;
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
 public class Wheel : Interactable
 {
+    // Tooltip & Particles
     public ParticleSystem steeringParticles;
     public GameObject tooltipObject;
     public Sprite toolipSprite;
-
     private GameObject toolTipInstance;
-    private ParticleSystem particlesInstance;
+
+    // Steering & pulse
+    public float pulseSpeed = 1.1f;
+
+    public bool requiresSteering { get; set; }
+    private Color originalColor;
+    private SpriteRenderer spriteRenderer;
+
+    // keys that are required to press to perfrom steering action
+    private string[] instructionKeys = { "w", "a", "s", "d" };
+    private GameObject instructionInstance;
+    public Sprite[] instructionSprites;
+
+    bool isInteracting = false;
+
+    public void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        originalColor = spriteRenderer.color;
+    }
+
+    public void Update()
+    {
+        Pulse();
+    }
+
+    public void Pulse()
+    {
+        if (requiresSteering || isInteracting)
+        {
+            float t = Mathf.PingPong(Time.time * pulseSpeed, 1f);
+            spriteRenderer.color = Color.Lerp(originalColor, Color.red, t);
+        }
+    }
 
     public override void Interact()
     {
-        particlesInstance = Instantiate(steeringParticles, transform.position, Quaternion.identity);
-        //particlesInstance.transform.Rotate(-90.0f, 0.0f, 0.0f);
+        if (isInteracting || !requiresSteering)
+        {
+            return;
+        }
+
+        // destroy tooltip instance, it obscures instructions
+        Destroy(toolTipInstance);
+
+        StartCoroutine(StartInteractionCycle());
+
+        requiresSteering = false;
+        isInteracting = true;
+    }
+
+    private IEnumerator StartInteractionCycle()
+    {
+        ShuffleInstructions();
+
+        int i = 0;
+        spawnActionKeyTooltip(i);
+
+        while (i < instructionKeys.Length)
+        {
+            if (Input.GetKeyDown(instructionKeys[i]))
+            {
+                // destroy last instruction instance before assigning new one
+                Destroy(instructionInstance);
+                instructionInstance = null;
+
+                i++;
+
+                if (i < instructionKeys.Length)
+                {
+                    spawnActionKeyTooltip(i);
+                }
+
+                // particles are removed automatically
+                Instantiate(steeringParticles, transform.position, Quaternion.identity);
+            }
+
+            yield return null;
+        }
+
+        // reset the color after the end of interaction (due to the pulse)
+        spriteRenderer.color = Color.white;
+
+        Destroy(instructionInstance);
+        isInteracting = false;
+    }
+
+    private void ShuffleInstructions()
+    {
+        var rnd = new System.Random();
+        var shuffled = instructionKeys.Zip(instructionSprites, (key, sprite) => (key, sprite))
+                                      .OrderBy(_ => rnd.Next())
+                                      .ToArray();
+
+        instructionKeys = shuffled.Select(x => x.key).ToArray();
+        instructionSprites = shuffled.Select(x => x.sprite).ToArray();
+    }
+
+    public void spawnActionKeyTooltip(int index)
+    {
+        Vector3 pos = new Vector3(transform.position.x + 0.5f, transform.position.y + 0.5f, transform.position.z);
+        instructionInstance = Instantiate(tooltipObject, pos, Quaternion.identity);
+        instructionInstance.GetComponent<SpriteRenderer>().sprite = instructionSprites[index];
     }
 
     public override void ShowTooltip(bool show)
     {
-        if (show)
+        if (show && requiresSteering && !isInteracting)
         {
             if (toolTipInstance != null)
             {
